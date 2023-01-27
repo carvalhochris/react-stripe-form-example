e26c6f346fb6055d1c6e0a0c08f0f00593cce9cb
Christopher <kriss17388+dev@gmail.com>
2023-01-27 17:49:52 +0000

    prettier

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export const API_URL ='https://warm-gorge-84676.herokuapp.com'
+export const API_URL = "https://warm-gorge-84676.herokuapp.com";
 
 export const api = axios.create({
-    baseURL: API_URL,
-    headers: {
-        "Content-type": "application/json"
-    }
+  baseURL: API_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
 });
 
- export default class ApiService{
-     static saveStripeInfo(data={}){
-         return api.post(`${API_URL}/payments/save-stripe-info/`, data)
-     }
- }
+export default class ApiService {
+  static saveStripeInfo(data = {}) {
+    return api.post(`${API_URL}/payments/save-stripe-info/`, data);
+  }
+}
